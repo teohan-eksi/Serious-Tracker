d12677a0d564530ac719818170909e1d2c8cfabb
teohan-eksi <75734275+teohan-eksi@users.noreply.github.com>
2021-06-03 15:24:01 +0300

    established an xhr load

--- renderer.js
+++ renderer.js
@@ -4,3 +4,19 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+
+document.getElementById("timer").addEventListener("click", ()=>{
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "pages/timer.html", true);
+
+  xhr.onload = function(){
+    if(this.status == 200){
+      
+    }
+  };
+
+  xhr.send();
+});
